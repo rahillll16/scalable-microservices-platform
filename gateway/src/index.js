4657a5c02b558c686014fd783e0071cd7f28e841
mohammad-rahil-ahmed <sahilrahil80230@gmail.com>
2026-06-15 18:12:52 +0530

    Day 4 - Order service, service communication and gateway integration

--- gateway/src/index.js
+++ gateway/src/index.js
@@ -12,11 +12,6 @@ app.get("/health", (req, res) => {
     });
 });
 
-app.use((req, res, next) => {
-    console.log("Gateway received:", req.method, req.url);
-    next();
-});
-
 app.use(
     "/users",
     createProxyMiddleware({
