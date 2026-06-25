c085bb58a87d1462b2c6e913ee040660aa17e8f9
mohammad-rahil-ahmed <sahilrahil80230@gmail.com>
2026-06-25 16:03:01 +0530

    retry threshold for login updated in gateway

--- gateway/src/index.js
+++ gateway/src/index.js
@@ -20,8 +20,8 @@ const globalLimiter = rateLimit({
 
 // creating login limiter
 const loginLimiter = rateLimit({
-    windowMs: 10*60*1000,
-    max: 5,
+    windowMs: 2*60*1000,
+    max: 20,
     message: {
         success: false,
         message: "Too many login attempts. Try again in 15 minutes."
